4681b1756596eb5a088d51c3b66ec8beb2e4bed7
Andreas Källberg <anka.213@gmail.com>
2021-12-08 11:28:46 +0800

    Add the "findtheinfo" function

--- src/WordNet/DB.hs
+++ src/WordNet/DB.hs
@@ -11,6 +11,7 @@ import Paths_wordnet (getDataFileName)
 
 foreign import ccall "wn_init_wordnet" init_wordnet :: CString -> IO CBool
 foreign import ccall "morphstr"        morphstr     :: CString -> CInt -> IO CString
+foreign import ccall "findtheinfo"     findtheinfo_ :: CString -> CInt -> CInt -> CInt -> IO CString
 
 initialise :: IO Bool
 initialise = do
@@ -31,6 +32,36 @@ ensureInit = do
         pure ()
 
 data POS = Any | Noun | Verb | Adj | Adv deriving (Show, Enum)
+
+data Search =
+ UNKNOWN_SEARCH
+ | ANTPTR          -- 1    /* ! */
+ | HYPERPTR        -- 2    /* @ */
+ | HYPOPTR         -- 3    /* ~ */
+ | ENTAILPTR       -- 4    /* * */
+ | SIMPTR          -- 5    /* & */
+
+ | ISMEMBERPTR     -- 6    /* #m */
+ | ISSTUFFPTR      -- 7    /* #s */
+ | ISPARTPTR       -- 8    /* #p */
+
+ | HASMEMBERPTR    -- 9    /* %m */
+ | HASSTUFFPTR     -- 10    /* %s */
+ | HASPARTPTR      -- 11    /* %p */
+
+ | MERONYM         -- 12    /* % (not valid in lexicographer file) */
+ | HOLONYM         -- 13    /* # (not valid in lexicographer file) */
+ | CAUSETO         -- 14    /* > */
+ | PPLPTR          -- 15    /* < */
+ | SEEALSOPTR      -- 16    /* ^ */
+ | PERTPTR         -- 17    /* \ */
+ | ATTRIBUTE       -- 18    /* = */
+ | VERBGROUP       -- 19    /* $ */
+ | DERIVATION      -- 20    /* + */
+ | CLASSIFICATION  -- 21    /* ; */
+ | CLASS           -- 22    /* - */
+  deriving (Show, Enum)
+
 
 morph1 :: Text -> POS -> IO (NonEmpty Text)
 morph1 w p = addFirst <$> morph w p
@@ -54,6 +85,21 @@ morph' w i = do
     else do
         rest <- morph' nullPtr i
         pure $ result : rest
+
+
+
+findtheinfo :: Text -> POS -> Search -> Maybe Int -> IO Text
+findtheinfo w pos search whichSense = do
+    ensureInit
+    s <- newCString $ unpack w
+    result <- findtheinfo_ s (fromIntegral $ fromEnum pos) (fromIntegral $ fromEnum search) (maybe 0 fromIntegral whichSense)
+    if result == nullPtr then
+        pure $ pack ""
+    else
+        pack <$> peekCString result
+
+getDerivations :: Text -> IO Text
+getDerivations w = findtheinfo w Verb DERIVATION Nothing
 
 data WordNetException
     = CannotInitialise
